40c8d86830464a93bb250d5a33bd9a9246b59fc0
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 21:26:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,7 +6,8 @@
 
 def square_array(array)
   new_array = []
-  array.each do { |number| new_array.push(number ** 2)}
+  array.each do |number| 
+  new_array.push(number ** 2)
   return new_array
 end
 
