5066fd5b6b251274f61ebfa3efb8b2fc6ce7fac8
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 21:47:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,14 @@
-def square_array(array)
-  new_array = []
-  array.each { |number| new_array.push(number ** 2)}
-  return new_array
-end
+# def square_array(array)
+#   new_array = []
+#   array.each { |number| new_array.push(number ** 2)}
+#   return new_array
+# end
 
 def square_array(array)
   new_array = []
   array.each do |number| 
   new_array << (number * number)
   end
-return new_array
+  return new_array
 end
 
