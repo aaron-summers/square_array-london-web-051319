ceae5b0a7217ebac3964107b90ce59d0e799ead0
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 21:26:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,12 @@
+# def square_array(array)
+#   new_array = []
+#   array.each { |number| new_array.push(number ** 2)}
+#   return new_array
+# end
+
 def square_array(array)
   new_array = []
   array.each { |number| new_array.push(number ** 2)}
   return new_array
 end
+
